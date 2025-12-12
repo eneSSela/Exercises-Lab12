34fb7feecd6c9dccbff315e5391b5fb4060f62f2
Enes-Sela <enes.sela@studio.unibo.it>
2025-12-12 22:03:50 +0100

    Modify GUI

--- 122-es2/src/main/java/it/unibo/es2/GUI.java
+++ 122-es2/src/main/java/it/unibo/es2/GUI.java
@@ -17,7 +17,6 @@ public final class GUI extends JFrame {
     @Serial
     private static final long serialVersionUID = 1L;
     private final Map<JButton, Pair<Integer, Integer>> buttons = new LinkedHashMap<>();
-    //private final Logics logics;
 
     /**
      * Constructs a GUI with the specified size.
@@ -25,7 +24,7 @@ public final class GUI extends JFrame {
      * @param size the size of the grid
      */
     public GUI(final int size) {
-        //this.logics = new LogicsImpl(size);
+        final Logics logics = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(100 * size, 100 * size);
         // Layout
@@ -37,9 +36,9 @@ public final class GUI extends JFrame {
                 final JButton button = new JButton(" ");
                 button.addActionListener(e -> {
                     final Pair<Integer, Integer> buttonPosition = buttons.get(button);
-                    button.setText(buttonPosition.toString());
-                    if (buttonPosition.equals(new Pair<>(0, 0))) {
-                        // System.exit(1); // Too brutal!
+                    logics.hit(buttonPosition.x(), buttonPosition.y());
+                    button.setText(logics.valueAt(buttonPosition.x(), buttonPosition.y()));
+                    if (logics.toQuit()) {
                         dispose();
                     }
                 });
