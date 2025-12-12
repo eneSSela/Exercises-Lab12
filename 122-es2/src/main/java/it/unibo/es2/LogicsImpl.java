daff91430057f7d0386ddf0785ab789930d984df
Enes-Sela <enes.sela@studio.unibo.it>
2025-12-12 21:21:14 +0100

    Create LogicsImpl Class

---
+++ 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
@@ -0,0 +1,31 @@
+package it.unibo.es2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementation of the Logics Interface.
+ */
+public class LogicsImpl implements Logics {
+
+    private final List<List<Boolean>> grid = new ArrayList<>();
+
+    @Override
+    public String valueAt(int row, int col) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'valueAt'");
+    }
+
+    @Override
+    public void hit(int row, int col) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hit'");
+    }
+
+    @Override
+    public boolean toQuit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+    }
+    
+}
